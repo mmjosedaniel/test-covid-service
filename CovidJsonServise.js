e1a3e9de9d3e12af0b8a97f2eb8e3d6e863f9786
Daniel Martínez <mmjosedaniel@hotmail.com>
2020-09-27 15:07:43 -0500

    Add: end point added.

--- CovidJsonServise.js
+++ CovidJsonServise.js
@@ -1,14 +1,40 @@
 const fetch = require('node-fetch');
+
+
 class CovidJsonServise {
     constructor() {
         this.URI = 'https://www.datos.gov.co/resource/gt2j-8ykr.json';
     }
 
-    async getMaleCases() {
+     async fetchCovidCases() {
         const response = await fetch(this.URI);
         const cases = await response.json();
-        console.log(cases)
-        return cases;
+        return cases
+    }
+
+    async getMaleCases() {
+        const cases = await this.fetchCovidCases();
+        return cases.filter(val => val.sexo === 'M');
+    }
+
+    async getFemaleCases() {
+        const cases = await this.fetchCovidCases();
+        return cases.filter(val => val.sexo === 'F');
+    }
+
+    async getZeroToTwenty() {
+        const cases = await this.fetchCovidCases();
+        return cases.filter(val => val.edad >= '0' && val.edad < '20');
+    }
+
+    async getTwentyToForty() {
+        const cases = await this.fetchCovidCases();
+        return cases.filter(val => val.edad >= '20' && val.edad < '40');
+    }
+
+    async getFortyAndPlus() {
+        const cases = await this.fetchCovidCases();
+        return cases.filter(val => val.edad >= '40');
     }
 }
 
--- routes/filters.js
+++ routes/filters.js
@@ -5,10 +5,22 @@ const CovidJsonServise = require('../CovidJsonServise');
 const covidJsonServise = new CovidJsonServise();
 
 
-router.get('/', async (req, res) => {
-	const result = await covidJsonServise.getMaleCases();
-	console.log(result);
-	res.json(result);
+router.get('/:filters', async (req, res) => {
+	const { filters } = req.params;
+	switch(filters) {
+		case 'male':
+			return res.json(await covidJsonServise.getMaleCases());
+		case 'female':
+			return res.json(await covidJsonServise.getFemaleCases());
+		case 'zerototwenty':
+			return res.json(await covidJsonServise.getZeroToTwenty());
+		case 'twentytoforty':
+			return res.json(await covidJsonServise.getTwentyToForty());
+		case 'fortyandplus':
+			return res.json(await covidJsonServise.getFortyAndPlus());
+	};
+
+	res.send('<h2>Error 404 for keyword: ' + filters + '<h2>')
 });
 
 module.exports = router;
