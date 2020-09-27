c8da3ecd234d1d15c1720d35d971982ad8f18b43
Daniel Martínez <mmjosedaniel@hotmail.com>
2020-09-27 15:46:36 -0500

    Fix: The function getInfoForEndPoint/1 was created to handle the information that is going to be sended throug the endpoint.

--- CovidJsonServise.js
+++ CovidJsonServise.js
@@ -1,40 +1,99 @@
 const fetch = require('node-fetch');
 
-
 class CovidJsonServise {
     constructor() {
         this.URI = 'https://www.datos.gov.co/resource/gt2j-8ykr.json';
     }
-
+    /**
+     * Get all the COVID-19 cases from the COVID-19 API 
+     * https://www.datos.gov.co/resource/gt2j-8ykr.json
+     * 
+     * @return json object with al the COVID-19 cases.
+     */
      async fetchCovidCases() {
         const response = await fetch(this.URI);
         const cases = await response.json();
         return cases
     }
 
+    /**
+     * Filters by sex to show oly the male cases.
+     * 
+     * @return Json object with the male cases.
+     */
     async getMaleCases() {
         const cases = await this.fetchCovidCases();
         return cases.filter(val => val.sexo === 'M');
     }
 
+    /**
+     * Filters by sex to show only de female cases.
+     * 
+     * @return Json object with the female cases.
+     */
     async getFemaleCases() {
         const cases = await this.fetchCovidCases();
         return cases.filter(val => val.sexo === 'F');
     }
 
+    /**
+     * Filters by age to show cases from zero to twenty years old people
+     * 
+     * @return json object with the cases from zero to twenty years old people
+     */
     async getZeroToTwenty() {
         const cases = await this.fetchCovidCases();
         return cases.filter(val => val.edad >= '0' && val.edad < '20');
     }
 
+    /**
+     * Filters by age to show cases from twenty to forty years old people
+     * 
+     * @return json object with the cases from twenty to forty years old people
+     */
     async getTwentyToForty() {
         const cases = await this.fetchCovidCases();
         return cases.filter(val => val.edad >= '20' && val.edad < '40');
     }
 
+    /**
+     * Filters by age to show cases from forty and plus years old people
+     * 
+     * @return json object with the cases from forty adn plus years old people
+     */
     async getFortyAndPlus() {
         const cases = await this.fetchCovidCases();
         return cases.filter(val => val.edad >= '40');
+    }
+
+    /**
+     * Gets the information to send throug the endpoint.
+     * This information deppends on a keyword.
+     * The possible keywords are:
+     * male, female, zerototwenty, twentytoforty, fortyandplus
+     * 
+     * @param filterKeyword the filter that is going to be used.
+     * 
+     * @return json object with the information for the endpoint.
+     */
+    async getInfoForEndPoint(filterKeyword) {
+        switch(filterKeyword) {
+            case 'male':
+                return await this.getMaleCases();
+            case 'female':
+                return await this.getFemaleCases();
+            case 'zerototwenty':
+                return await this.getZeroToTwenty();
+            case 'twentytoforty':
+                return await this.getTwentyToForty();
+            case 'fortyandplus':
+                return await this.getFortyAndPlus();
+        };
+    
+        return (
+            { 'error': 'Error 404', 'for keyword': filterKeyword,
+            'Possible keywords': 'male, female, zerototwenty, twentytoforty, fortyandplus'}
+        )
     }
 }
 
--- routes/filters.js
+++ routes/filters.js
@@ -5,22 +5,9 @@ const CovidJsonServise = require('../CovidJsonServise');
 const covidJsonServise = new CovidJsonServise();
 
 
-router.get('/:filters', async (req, res) => {
-	const { filters } = req.params;
-	switch(filters) {
-		case 'male':
-			return res.json(await covidJsonServise.getMaleCases());
-		case 'female':
-			return res.json(await covidJsonServise.getFemaleCases());
-		case 'zerototwenty':
-			return res.json(await covidJsonServise.getZeroToTwenty());
-		case 'twentytoforty':
-			return res.json(await covidJsonServise.getTwentyToForty());
-		case 'fortyandplus':
-			return res.json(await covidJsonServise.getFortyAndPlus());
-	};
-
-	res.send('<h2>Error 404 for keyword: ' + filters + '<h2>')
+router.get('/:filterKeyword', async (req, res) => {
+	const { filterKeyword } = req.params;
+	res.json(await covidJsonServise.getInfoForEndPoint(filterKeyword));
 });
 
 module.exports = router;
